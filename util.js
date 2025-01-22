2253f2487dbf12c73360c52350197016d986458b
asifichy <asifimtiyajchowdhury@gmail.com>
2025-01-22 11:16:03 +0600

    exception error

--- util.js
+++ util.js
@@ -6,7 +6,7 @@ function getRandomInt(min, max) {
 
 function doSomeHeavyTask() {
     const ms = getRandomInt([100, 150, 200, 300, 600, 500, 1000, 1400, 2500]);
-    const shouldThrwoError = getRandomInt([1, 2, 3, 4, 5, 6, 7, 8,]) === 8;
+    const shouldThrwoError = getRandomValue([1, 2, 3, 4, 5, 6, 7, 8,]) === 8;
     if(shouldThrwoError){
         const randomError = getRandomValue([
             "DB Payment Failure",
