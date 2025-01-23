bff9cfad93474d35e441250eb824d95337103408
asifichy <asifimtiyajchowdhury@gmail.com>
2025-01-23 12:15:31 +0600

    adding custom metrics for pro-server

--- index.js
+++ index.js
@@ -1,27 +1,58 @@
 const express = require('express');
+const client = require('prom-client'); //metric collection
+const responseTime = require('response-time');
 const { doSomeHeavyTask } = require('./util');
 
 const app = express();
 const PORT = process.env.PORT || 8000;
+
+const collectionDefualtMetrics = client.collectDefaultMetrics;
+
+collectionDefualtMetrics({ register: client.register });
 
 app.get("/", (req, res) => {
   return res.json({ message: `Hello World` });
 });
 
 app.get("/slow", async (req, res) => {
-  try{
+  try {
     const timeTaken = await doSomeHeavyTask();
-    return res.json({ 
-        status: "Success",
-        message: `Heavy task completed in ${timeTaken}ms` });
+    return res.json({
+      status: "Success",
+      message: `Heavy task completed in ${timeTaken}ms`
+    });
   }
-  catch(err){    
+  catch (err) {
     return res.status(500).json({
       status: "Failed",
       error: "Internal Server Error",
     })
   }
 });
+
+//historgram metreics
+const reqResTime = new client.Histogram({
+  name: 'http_express_req_res_time',
+  help: 'This tells how much time it takes',
+  labelNames: ["method", "route", "code"],
+  buckets: [1, 5, 15, 50, 100, 500, 1000],
+});
+
+app.use(responseTime((req, res, time) => {
+    reqResTime.labels({
+      method: req.method,
+      route: req.url,
+      code: res.statusCode,
+    }).observe(time);
+  })
+);
+
+//adding the metrics endpoint
+app.get("/metrics", async (req, res) => {
+  res.setHeader('Content-Type', client.register.contentType);
+  const metrics = await client.register.metrics();
+  res.send(metrics);
+})
 
 app.listen(PORT, () => {
   console.log(`Express Server is running at http://localhost:${PORT}`);
